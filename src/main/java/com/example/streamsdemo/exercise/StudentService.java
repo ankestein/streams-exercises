281f71bb4ffe8100a159f27f20c65e45a4ff3890
christophersiem <christopher.siem@gmail.com>
2021-05-29 12:35:55 +0200

    Update exercises

--- src/main/java/com/example/streamsdemo/exercise/StudentService.java
+++ src/main/java/com/example/streamsdemo/exercise/StudentService.java
@@ -4,7 +4,6 @@ import com.example.streamsdemo.model.Student;
 import com.example.streamsdemo.model.StudentDto;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,12 +14,12 @@ public class StudentService {
             Student.builder().name("Frank").location("Hamburg").age(42).build(),
             Student.builder().name("Maria").location("Köln").age(15).build(),
             Student.builder().name("Klaus").location("Frankfurt").age(22).build(),
+            Student.builder().name("Frida").location("Köln").age(77).build(),
             Student.builder().name("Franziska").location("München").age(45).build(),
             Student.builder().name("Anne").location("Frankfurt").age(50).build(),
             Student.builder().name("Hans").location("Hamburg").age(43).build(),
-            Student.builder().name("Klaus").location("Hamburg").age(17).build(),
-            Student.builder().name("Gerda").location("München").age(31).build(),
-            Student.builder().name("Frida").location("Köln").age(77).build()
+            Student.builder().name("Hugo").location("Hamburg").age(17).build(),
+            Student.builder().name("Gerda").location("München").age(31).build()
     ));
 
 
@@ -36,16 +35,20 @@ public class StudentService {
                 .filter(student -> student.getAge() <= maxAge).collect(Collectors.toList());
     }
 
-    // 3. Get the oldest student
-    public Student getOldestStudent(){
-        return students.stream()
-                .max(Comparator.comparing(Student::getAge)).get();
-    }
-
-    // 4. Write a method that maps a Student to a StudentDto
+    // 3. Write a method that maps a Student to a StudentDto
     public List<StudentDto> createStudentDto(){
         return students.stream()
                 .map(student -> StudentDto.builder().location(student.getLocation()).name(student.getName()).build())
                 .collect(Collectors.toList());
     }
+
+    // 4. Return a List with all names starting with F in uppercase and alphabetically sorted
+    public List<String> filterStudentsByName(){
+        return students.stream()
+                .filter(student -> student.getName().startsWith("F"))
+                .map(student -> student.getName().toUpperCase())
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
 }
